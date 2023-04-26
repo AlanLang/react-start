461ea77d398b9f7f51a17de9cb8adac0ea65f65e
alan <alan@fanruan.com>
2023-04-26 16:57:12 +0800

    chore:  react

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production"; // 或者 env.production
+  return {
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? false : "inline-source-map",
+    entry: "./src/index.tsx",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
     },
-    compress: true,
-    port: 9000,
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'swc-loader',
-          options: {
-            sync: true
-          }
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "swc-loader",
+            options: {
+              sync: true,
+            },
+          },
         },
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, 'public/index.html')
-    }),
-  ]
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: path.join(__dirname, "public/index.html"),
+      }),
+    ],
+  };
 };
