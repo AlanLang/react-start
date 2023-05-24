76701c303eed7ff08169f88b9f429da18d7cf7cb
alan <alan@fanruan.com>
2023-05-24 09:27:41 +0800

    chore:  add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { sum } from "./demo";
 import React from "react";
-
+import "./index.css";
 function App() {
-  return <div>{sum(1, 2)}</div>;
+  return <div className="text-red-600">{sum(1, 2)}</div>;
 }
 
 const root = createRoot(document.getElementById("app")!);
